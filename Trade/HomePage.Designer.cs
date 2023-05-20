9c71c892d00426f0292cfdf25dd3c12cc809c943
billiboba <romakor.19@yandex.ru>
2023-05-20 20:46:36 +0700

    Click DB

--- Trade/HomePage.Designer.cs
+++ Trade/HomePage.Designer.cs
@@ -35,6 +35,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.DeleteDb = new System.Windows.Forms.Button();
             this.ViewDb = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.saveInstrumentsBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -42,19 +43,22 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(93, 3);
+            this.dataGridView1.Location = new System.Drawing.Point(106, 4);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(715, 446);
+            this.dataGridView1.Size = new System.Drawing.Size(224, 595);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(14, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "Load DB";
             this.button1.UseVisualStyleBackColor = false;
@@ -63,9 +67,10 @@
             // DeleteDb
             // 
             this.DeleteDb.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.DeleteDb.Location = new System.Drawing.Point(12, 122);
+            this.DeleteDb.Location = new System.Drawing.Point(14, 163);
+            this.DeleteDb.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.DeleteDb.Name = "DeleteDb";
-            this.DeleteDb.Size = new System.Drawing.Size(75, 23);
+            this.DeleteDb.Size = new System.Drawing.Size(86, 31);
             this.DeleteDb.TabIndex = 2;
             this.DeleteDb.Text = "Delete DB";
             this.DeleteDb.UseVisualStyleBackColor = false;
@@ -74,24 +79,30 @@
             // ViewDb
             // 
             this.ViewDb.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ViewDb.Location = new System.Drawing.Point(12, 68);
+            this.ViewDb.Location = new System.Drawing.Point(14, 91);
+            this.ViewDb.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ViewDb.Name = "ViewDb";
-            this.ViewDb.Size = new System.Drawing.Size(75, 23);
+            this.ViewDb.Size = new System.Drawing.Size(86, 31);
             this.ViewDb.TabIndex = 3;
             this.ViewDb.Text = "View DB";
             this.ViewDb.UseVisualStyleBackColor = false;
             this.ViewDb.Click += new System.EventHandler(this.ViewDb_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // HomePage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(811, 450);
+            this.ClientSize = new System.Drawing.Size(1015, 600);
             this.Controls.Add(this.ViewDb);
             this.Controls.Add(this.DeleteDb);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "HomePage";
             this.Text = "HomePage";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -107,5 +118,6 @@
         private Button button1;
         private Button DeleteDb;
         private Button ViewDb;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Trade/HomePage.cs
+++ Trade/HomePage.cs
@@ -19,28 +19,35 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using System.Net;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Timers;
+using System.Threading;
+using Timer = System.Windows.Forms.Timer;
 
 namespace Trade
 {
     
     public partial class HomePage : Form
     {
+        private Timer timer = new Timer();
         public class CryptoCurrency
         {
-            //public string Name { get; set; }
             public string Symbol { get; set; }
-            public int Id { get; set; }
             public string Price { get; set; }
         }
         public HomePage()
         {
             InitializeComponent();
-            //SI.SaveInst();
-            
+            timer.Interval = 5000;
+
+            // Устанавливаем обработчик события таймера
+            timer.Tick += timer1_Tick;
+
+            // Запускаем таймер
+            timer.Start();
         }
         private async void button1_Click(object sender, EventArgs e)
         {
-            var connectionString = "Data Source=DESKTOP-CR5D09I\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=True";
+            var connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=True";
             using(var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -61,30 +68,49 @@ namespace Trade
                         foreach (var currency in currencies)
                         {
                             var query2 = $"INSERT INTO Currencies (symbol,price) VALUES('{currency.Symbol}','{currency.Price}');";
-
+                            //var query3 = "UPDATE Currencies SET (price) VALUE('{}')";
                             using (var command = new SqlCommand(query2, connection))
                             {
                                 command.ExecuteNonQuery();
                             }
                         }
                     }
-                }
-                
+                }  
             }
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            
+            string connectionstring = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionstring);
+            
+            int rowIndex = e.RowIndex;
+
+            for(int i = e.RowIndex+1; i < dataGridView1.Rows.Count; i++)
+            {
+                string id = dataGridView1.Rows[rowIndex].Cells["symbol"].Value.ToString(); // id - имя столбца с ID в таблице
+
+                string query = $"SELECT * FROM Currencies WHERE symbol = '{id}'";
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = new SqlCommand(query, connection);
+
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet, "Currencies");
+
+                dataGridView1.DataSource = dataSet.Tables["Currencies"];
+            }
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             dataGridView1.AutoResizeColumns();
         }
 
         private void DeleteDb_Click(object sender, EventArgs e)
         {
-            string connectionString = "Data Source=DESKTOP-CR5D09I\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=True";
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
-            string sql = "DELETE FROM Currencies WHERE Id>0";
+            string sql = @"DELETE FROM Currencies";
             SqlCommand command = new SqlCommand(sql, connection);
             command.ExecuteNonQuery();
             
@@ -101,11 +127,10 @@ namespace Trade
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             connection.Close();
         }
-
         private void ViewDb_Click(object sender, EventArgs e)
         {
-            string query = "SELECT * FROM Currencies";
-            string connectionString = "Data Source=DESKTOP-CR5D09I\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=True";
+            string query = "SELECT symbol FROM Currencies";
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=True";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -118,6 +143,24 @@ namespace Trade
             }
 
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=SaveBinance;TrustServerCertificate=True;Integrated Security=Tru"))
+            {
+                // Открываем соединение
+                connection.Open();
+
+                // Создаем объект команды SQL для выполнения запроса
+                SqlCommand command = new SqlCommand("UPDATE Currencies SET price", connection);
+
+                // Выполняем запрос
+                command.ExecuteNonQuery();
+
+                // Закрываем соединение
+                connection.Close();
+            }
         }
     }
     
